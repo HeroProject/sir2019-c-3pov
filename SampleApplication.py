ba03e3451acc8698448169884b7dd5eb1c85e46e
neilmizzi <mizzin@hotmail.com>
2019-11-19 11:44:25 +0100

    added dialogflow sample

--- SampleApplication.py
+++ SampleApplication.py
@@ -1,10 +1,12 @@
+from time import sleep
+
 import AbstractApplication as Base
 
 
 class SampleApplication(Base.AbstractApplication):
     def main(self):
         self.setLanguage('en-US')
-        self.sayAnimated('Hello, world!')
+        self.sayAnimated('Hello people')
         sleep(2)
 
     def onRobotEvent(self, event):
